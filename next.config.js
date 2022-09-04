dc598fca627da0defd79a87a72477dde9fe580d1
ThanhHauC <nguyenthanhhau.contact@gmail.com>
2022-09-04 19:57:18 +0700

    update #6

--- next.config.js
+++ next.config.js
@@ -1,2 +1,14 @@
-const withImages = require("next-images");
-module.exports = withImages();
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: "file-loader",
+          },
+        ],
+      },
+    ],
+  },
+};
